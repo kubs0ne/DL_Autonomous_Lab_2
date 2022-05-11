8bc41e8a73238ca462e71fa6d2ccafe74b6b7456
Bennetz <bernhard.boeckenhoff@edu.udo>
2022-05-11 12:25:12 +0200

    Test files

---
+++ Experiments/2/ex2.py
@@ -0,0 +1,64 @@
+import inspect
+import os
+import sys
+import time
+
+import tensorflow as tf
+from keras import optimizers
+from keras.callbacks import EarlyStopping
+from keras.layers import ( Dense, Flatten)
+from keras.models import Model, Sequential
+from tensorflow.keras import applications
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentparentdir = os.path.dirname(parentdir)
+sys.path.insert(0, parentparentdir)
+import DataGenerator
+import ModelEvaluator
+
+num_classes = 29
+img_width, img_height = 256, 256
+batch_size = 64
+epochs = 10
+
+train_generator, validation_generator, test_generator = DataGenerator.data_Gens(parentparentdir, img_height, img_width, batch_size)
+
+model = applications.InceptionV3(weights = "imagenet", include_top=False, input_shape = (img_width, img_height, 3))
+
+for layer in model.layers:
+    layer.trainable = False
+
+#Adding custom Layers
+x = model.output
+x = Flatten()(x)
+x = Dense(512, activation="relu")(x)
+x = Dense(512, activation="relu")(x)
+predictions = Dense(num_classes, activation="softmax")(x)
+
+# creating the final model
+model_final = Model(model.input, predictions)
+
+# compile the model
+model_final.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
+model_final.summary()
+
+# Train the model
+t0 = time.time()
+STEP_SIZE_TRAIN = train_generator.n // train_generator.batch_size
+STEP_SIZE_VAL = validation_generator.n // validation_generator.batch_size
+
+early = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=10, verbose=1, mode='auto',
+                      restore_best_weights=True)
+
+history = model_final.fit_generator(
+    generator=train_generator,
+    steps_per_epoch= STEP_SIZE_TRAIN,
+    validation_data=validation_generator,
+    validation_steps= STEP_SIZE_VAL,
+    epochs=epochs
+)
+
+print('Model trained in {:.1f}min'.format((time.time() - t0) / 60))
+
+ModelEvaluator.evaluate_model(model_final, history, validation_generator)
