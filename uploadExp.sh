3fbfa9b3d075b5f135812d67766209bdddf8c765
Bennetz <bernhard.boeckenhoff@edu.udo>
2022-05-17 16:36:33 +0200

    weird script

--- uploadExp.sh
+++ uploadExp.sh
@@ -1,2 +1,2 @@
-scp -r Experiments/14 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab2/Experiments #Upload the current experiment
-ssh nct01029@plogin1.bsc.es 'sbatch AutoLab2/Experiments/14/ex14.sh' #Submit sbatch
+scp -r Experiments/23 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab2/Experiments #Upload the current experiment
+ssh nct01029@plogin1.bsc.es 'sbatch AutoLab2/Experiments/23/ex23.sh' #Submit sbatch
